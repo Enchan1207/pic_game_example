70f6fb2cac41126be2c0e7aa54ebe4d56381de94
Enchan <enchant0130@gmail.com>
2024-01-15 22:58:33 +0900

    [Update #1] joystick positioning process

--- hardware/joystick.c
+++ hardware/joystick.c
@@ -24,8 +24,11 @@ void joystick_requireUpdate(void) {
  * @note 最後にAD変換した際の結果から位置を算出します。更新するには `joystick_requireUpdate` を呼び出してください。
  */
 void joystick_getPosition(int8_t* x, int8_t* y) {
-    uint8_t rawX = adc_getValue(JoyStickX);
-    uint8_t rawY = adc_getValue(JoyStickY);
+    // ADCから値を持ってきて上位4bitを持ってくる y軸は上下逆なのでflip
+    uint8_t rawX = (adc_getValue(JoyStickX) >> 4);
+    uint8_t rawY = 0x0F - (adc_getValue(JoyStickY) >> 4);
 
-    // TODO: 変換式
+    // 値を -7 ~ +7 にスケーリング
+    *x = (int8_t)rawX - 7;
+    *y = (int8_t)rawY - 7;
 }

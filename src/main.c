099c542b51a7c452645fa699cb1411f0551e1a1b
Enchan <enchant0130@gmail.com>
2024-01-15 17:35:13 +0900

    [Modify #1] joystick monitoring process

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 //
 #include <hardware/display.h>
 #include <hardware/distsens.h>
+#include <hardware/joystick.h>
 #include <stdbool.h>
 #include <string.h>
 #include <xc.h>
@@ -18,6 +19,9 @@ void main(void) {
 
     // 距離センサを初期化
     distsens_init();
+
+    // ジョイスティックを初期化
+    joystick_init();
 
     // ディスプレイモジュールを初期化
     display_init();
@@ -35,22 +39,38 @@ void main(void) {
     uint8_t* displayBuffer = display_getDrawBuffer();
 
     // オブジェクト初期化
-    struct RenderObject* obj = renderObjects;
-    obj->isVisible = true;
-    obj->height = 1;
+    struct RenderObject* distobj = renderObjects;
+    struct RenderObject* joyXobj = renderObjects + 1;
+    struct RenderObject* joyYobj = renderObjects + 2;
+    distobj->isVisible = true;
+    joyXobj->isVisible = true;
+    joyYobj->isVisible = true;
+    distobj->height = 1;
+    joyXobj->height = 1;
+    joyYobj->height = 1;
+    joyXobj->sy = 1;
+    joyYobj->sy = 2;
 
     // 表示を開始
     display_setVisible(true);
 
     while (true) {
-        // 距離センサ値の更新を要求
-        distsens_requireUpdate(DistanceSensor);
+        // 各ペリフェラルの更新を要求
+        distsens_requireUpdate();
+        joystick_requireUpdate();
 
         // オブジェクトをレンダリング
         renderer_renderObjects(displayBuffer);
 
-        // 最後に取得した値を反映
+        // ジョイスティックの値を反映
+        int8_t x = 0;
+        int8_t y = 0;
+        joystick_getPosition(&x, &y);
+        joyXobj->width = x;
+        joyYobj->width = y;
+
+        // 距離センサの値を反映
         uint16_t distance = distsens_getDistance();
-        obj->width = distance >> 13;
+        distobj->width = distance >> 13;
     }
 }

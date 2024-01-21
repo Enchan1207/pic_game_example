9ffcb20a16b7d7158e805418828055c8c120b6a7
Enchan <enchant0130@gmail.com>
2024-01-21 20:16:50 +0900

    [Update #22] state-changing

--- src/wall_escape/main.c
+++ src/wall_escape/main.c
@@ -5,14 +5,14 @@
 #include <string.h>
 #include <xc.h>
 
-#include "gametick.h"
+#include "core/gametick.h"
+#include "core/renderer.h"
 #include "hardware/button.h"
 #include "hardware/display.h"
-#include "hardware/distsens.h"
 #include "hardware/joystick.h"
 #include "hardware/random.h"
-#include "renderer.h"
 #include "wall_escape/collision.h"
+#include "wall_escape/state.h"
 #include "wall_escape/wall.h"
 
 void main(void) {
@@ -22,79 +22,109 @@ void main(void) {
     // LCD制御機能を無効化
     LCDEN = 0;
 
-    // ゲームティック初期化
+    // 各モジュールの初期化
+    button_init();
     gametick_init();
-
-    // 距離センサを初期化
-    distsens_init();
-
-    // ジョイスティックを初期化
     joystick_init();
-
-    // ディスプレイモジュールを初期化
     display_init();
-
-    // オブジェクトレンダラ初期化
     renderer_init();
-
-    // 乱数生成器初期化
     random_init();
-
-    // 壁生成器初期化
     wall_init();
 
     // グローバル割り込み有効化
     INTCONbits.GIE = 1;
     INTCONbits.PEIE = 1;
 
-    // 表示の有効化
+    // 表示の有効化, ゲームティック開始
     display_setVisible(true);
-
-    // ゲームティックを開始
     gametick_start();
 
-    // 乱数シード設定
-    random_initSeed();
+    // ゲームステート
+    GameStateType gameState = Ready;
 
     // ジョイスティックの値
     int8_t stickX = 0;
     int8_t stickY = 0;
 
-    // 壁の出現頻度と通過した壁の数
-    uint8_t wallSpawnRate = 128;
-
-    // オブジェクト初期化
+    // プレイヤーオブジェクト
     struct RenderObject* player = renderer_getRenderObjectByID(0x07);
     player->isVisible = true;
     player->type = PlayerObject;
 
+    // ゲームループ
     while (true) {
-        // ゲームティックに入ったら
-        if (gametick_isTickEntered()) {
-            // オブジェクトを描画し、バッファの参照を切り替える
-            uint8_t* displayBuffer = display_getDrawBuffer();
-            memset(displayBuffer, 0x00, 8);
-            renderer_drawObjects(displayBuffer);
-            display_switchBuffer();
+        // オブジェクトを描画し、バッファの参照を切り替える
+        uint8_t* displayBuffer = display_getDrawBuffer();
+        memset(displayBuffer, 0x00, 8);
+        displayBuffer[0] = gameState;
+        renderer_drawObjects(displayBuffer);
+        display_switchBuffer();
 
-            // プレイヤーの位置を更新
-            joystick_requireUpdate();
-            joystick_getPosition(&stickX, &stickY);
-            player->sx = (stickX + 7) >> 1;
-            player->sy = (stickY + 7) >> 1;
+        // プレイヤーの位置を更新
+        joystick_requireUpdate();
+        joystick_getPosition(&stickX, &stickY);
+        player->sx = (stickX + 7) >> 1;
+        player->sy = (stickY + 7) >> 1;
 
-            // 壁の位置を更新
-            wall_updateWalls();
+        // ゲームステートによって分岐
+        switch (gameState) {
+            case Ready:
+                // キー入力待ち
+                if (!button_isTyped(RedButton)) {
+                    break;
+                }
 
-            // TODO: 当たり判定とステート分岐
-            if (isPlayerCollidedWall()) {
-                gametick_stop();
-            }
+                // 乱数シードを設定
+                random_initSeed();
 
-            // スポーンレートごとに壁オブジェクトを生成
-            if ((gametick_getTickCount() % wall_getSpawnRate()) == 0) {
-                wall_generateNewWall();
-            }
+                // プレイヤーを表示
+                player->isVisible = true;
+
+                gameState = Playing;
+                break;
+
+            case Playing:
+                // 壁の位置を更新
+                wall_updateWalls();
+
+                // 壁にぶつかったらステート分岐
+                if (isPlayerCollidedWall()) {
+                    gameState = Gameover;
+                    break;
+                }
+
+                // スポーンレートごとに壁オブジェクトを生成
+                if ((gametick_getTickCount() % wall_getSpawnRate()) == 0) {
+                    wall_generateNewWall();
+                }
+                break;
+
+            case Gameover:
+                // キー入力待ち
+                if (!button_isTyped(RedButton)) {
+                    break;
+                }
+
+                // オブジェクトテーブルをクリア
+                for (uint8_t i = 0; i < RENDERER_MAX_OBJECT; i++) {
+                    struct RenderObject* obj = renderer_getRenderObjectByID(i);
+                    obj->isVisible = false;
+                    obj->sx = -1;
+                    obj->sy = -1;
+                }
+
+                // TODO: スコアを表示する
+
+                // キー入力待ち
+                if (!button_isTyped(RedButton)) {
+                    break;
+                }
+
+                gameState = Ready;
+                break;
         }
+
+        // ゲームティック同期
+        gametick_sync();
     }
 }
